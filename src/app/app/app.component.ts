95bc7cf067ca161bb0b5712f91b445302b4446a7
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-07 10:15:52 +0300

    some non-logged UX block + hints

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { App } from '../services/app';
 import { Router } from '@angular/router';
 import { THEME } from '../services/theme-info';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-app',
@@ -11,10 +12,16 @@ import { THEME } from '../services/theme-info';
 export class AppComponent implements OnInit {
   @Input() app: App;
   public theme = THEME;
+  public hint: string;
   constructor(private router: Router,
+              private auth: AuthService
               ) { }
   toggleModal(): void {
-    this.router.navigate(['/app/' + this.app.id]);
+    if (this.auth.checkRights() !== 'non-logged') {
+      this.router.navigate(['/app/' + this.app.id]);
+    } else {
+      this.hint = 'You are not logged to check this info';
+    }
   }
   ngOnInit() {
   }
