98edfd05eb545b2f572499434accaa78bd20e4ed
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-11 15:44:06 +0300

    headers fix

--- src/app/shared-services/http-service/http-service.service.ts
+++ src/app/shared-services/http-service/http-service.service.ts
@@ -8,24 +8,30 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class HttpServiceService {
   constructor(private http: HttpClient) { }
   get(url: string,
-      myHeaders = [
-        'Content-Type',
-        'application/json'
-      ]
+      myHeaders = {
+    'Content-Type': 'application/json'
+      }
       ): Observable<any> {
     const headers = new HttpHeaders();
-    headers.set(myHeaders[0], myHeaders[1]);
+    for (const key of Object.keys(myHeaders)) {
+      for (const value of Object.values(myHeaders)) {
+        headers.set(key, value);
+      }
+    }
     return this.http.get(url, {headers: headers});
   }
   post(url: string,
        data = {},
-       myHeaders = [
-         'Content-Type',
-         'application/json'
-       ],
+       myHeaders = {
+         'Content-Type': 'application/json'
+       },
   ): Observable<any> {
     const headers = new HttpHeaders();
-    headers.set(myHeaders[0], myHeaders[1]);
+    for (const key of Object.keys(myHeaders)) {
+      for (const value of Object.values(myHeaders)) {
+        headers.set(key, value);
+      }
+    }
     return this.http.post(url, data, {headers: headers});
   }
 }
